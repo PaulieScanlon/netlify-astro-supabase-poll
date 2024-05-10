f7e9c6ffe852479a48c8d0552f92df6a890099b1
Paul Scanlon <pauliescanlon@gmail.com>
2024-05-10 12:39:51 -0500

    feat: add comments to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,19 @@
 export interface PollProps {
-  submitted: string | null;
-  results: { [key: string]: number };
-  maxValue: Number;
-  totalVotes: Number;
+  // These are the props the form component receives
+  submitted: string | null; // This determines if the form or results are displayed
+  results: { [key: string]: number }; // This is a reduced object that calculates the amount of times each result has been submitted to the database
+  maxValue: Number; // This value is calculated by reducing all the results from the database
+  totalVotes: Number; // This is the total amount of results from the database
 }
 
 export interface FormConfig {
-  form_name: string;
-  fields: Field[];
+  // This the shape of the form components config object
+  form_name: string; // The name of the form.
+  fields: Field[]; //  An array of the Field shape
 }
 
 export interface Field {
-  id: string;
-  label: string;
+  // This is the shape of the form fields
+  id: string; // The id of the input field
+  label: string; // The label of the input field
 }
